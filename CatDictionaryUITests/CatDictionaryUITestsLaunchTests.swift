7bbe573c7f798f5eb35abf5d8dd464512e748cae
Jayden Jang <newcephei@icloud.com>
2023-05-07 19:23:50 +0900

    Initial Commit

---
+++ CatDictionaryUITests/CatDictionaryUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CatDictionaryUITestsLaunchTests.swift
+//  CatDictionaryUITests
+//
+//  Created by Jayden Jang on 2023/05/07.
+//
+
+import XCTest
+
+final class CatDictionaryUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
