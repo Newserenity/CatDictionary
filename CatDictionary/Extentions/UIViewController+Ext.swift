f27fba5a4e8cbde4490afc5723e4f3f8aada521e
Jayden H Jang <Newcephei@icloud.com>
2023-05-22 11:01:18 +0900

    if debug mode fix

--- CatDictionary/Extentions/UIViewController+Ext.swift
+++ CatDictionary/Extentions/UIViewController+Ext.swift
@@ -7,7 +7,19 @@
 
 import UIKit
 
-#if DEBUG // 전처리기
+extension UIViewController {
+    func configNavbar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        navigationController?.navigationBar.tintColor = .brown
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
+
+#if DEBUG
 
 import SwiftUI
 
@@ -26,16 +38,6 @@ extension UIViewController {
     
     func getPreview() -> some View {
         VCRepresentable(viewController: self)
-    }
-    
-    func configNavbar() {
-        let appearance = UINavigationBarAppearance()
-        appearance.configureWithOpaqueBackground()
-        navigationController?.navigationBar.tintColor = .brown
-        navigationController?.navigationBar.standardAppearance = appearance
-        navigationController?.navigationBar.compactAppearance = appearance
-        navigationController?.navigationBar.scrollEdgeAppearance = appearance
-        navigationController?.navigationBar.prefersLargeTitles = true
     }
 }
 
